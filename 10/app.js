1ef902449b8abaf22567ae52b2c1336bcf47e297
HWOWON SEO <59409320+hwon22@users.noreply.github.com>
2020-09-19 22:30:12 +0900

    nodejs_cookie,session 예제

---
+++ 10/app.js
@@ -0,0 +1,26 @@
+//세션기본
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session); 
+const app = express();
+
+app.use(session({  
+  secret: 'asdfaf', 
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}));
+
+app.get('/', (req, res, next) => { 
+  console.log(req.session);
+  if(!req.session.num){
+    req.session.num = 1;
+  } else {
+    req.session.num = req.session.num + 1;
+  }
+  res.send(`Number : ${req.session.num}`);
+});
+
+app.listen(3000, () => {
+  console.log('Connected express server at localhost!');
+});
